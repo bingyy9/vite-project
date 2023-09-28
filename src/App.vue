e0025322fe971e7a7fd584212d33c0c75e17f551
Bing Yang (bingy) <bingy@cisco.com>
2023-09-28 21:44:42 +0800

    cache code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
-<script>
-export default {
-  data(){
-    return{
-      msg:"test2"
-    }
-  }
-}
+<script setup>
+import HelloWorld2 from './components/HelloWorld2.vue';
+
+// export default {
+//   data(){
+//     return{
+//       msg:"test2",
+//       rawHtml:"<a href='https://www.baidu.com'>baidu</a>"
+//     }
+//   }
+// }
 </script>
 
 <template>
-  <h3>模板语法</h3>
+  <!-- <h3>模板语法</h3>
   <p>{{ msg }}</p>
+  <p v-html="rawHtml"></p> -->
+  <HelloWorld2/>
 </template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    data() {
+      return {
+        msg: "HelloWorld"
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+</template>
